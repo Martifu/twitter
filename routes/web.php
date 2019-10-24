ed09939c55e2f5589c8286d6d1bfa82e6abea1e1
DaniGadget18 <dani_181298@hotmail.com>
2019-10-24 14:38:25 -0500

    Nav casi lista

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/base', function () {
+    return view('base');
+});
+
+Route::get('/Inicio', function (){
+   return view('Inicio');
 });
