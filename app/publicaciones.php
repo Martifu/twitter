a818b66764474879850cd86b555614b90212a98d
DaniGadget18 <dani_181298@hotmail.com>
2019-11-06 19:52:48 -0600

    noticias

---
+++ app/publicaciones.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class publicaciones extends Model
+{
+    protected $table = "publicaciones";
+    protected $primaryKey = "id";
+}
